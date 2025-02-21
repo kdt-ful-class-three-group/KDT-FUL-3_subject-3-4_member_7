d763d9f1ece09ee99364be10f78c4e4562e4a313
윤종환 <yunjonghwan99@gmail.com>
2025-02-21 16:45:48 +0900

    파일 읽어오는 코드 작성

---
+++ src/function/readDataFile.js
@@ -0,0 +1,9 @@
+import fs from "fs";
+
+function readDataFile() {
+  const dataFile = fs.readFileSync("../../data.JSON");
+  const data = dataFile.toString();
+  return data;
+}
+
+export { readDataFile }
