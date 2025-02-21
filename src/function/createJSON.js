b82bf397db6fe66422b1019bdf0275defb76a751
윤종환 <yunjonghwan99@gmail.com>
2025-02-21 16:09:31 +0900

    데이터를 JSON 문자열로 만든다.

--- src/function/createJSON.js
+++ src/function/createJSON.js
@@ -1,8 +1,10 @@
 import fs from "fs";
+import { dataToJsonStringify } from "./dataToJsonStringify.js";
 
 function createJSON(data) {
+  const dataJSON = dataToJsonStringify(data);
   //* data.JSON으로 파일 생성
-  fs.writeFileSync("data.JSON", );
+  fs.writeFileSync("data.JSON", dataJSON);
 }
 
 export { createJSON }
